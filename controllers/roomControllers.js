fbbd05aaaa995d045f783a6a66b38a330b542833
syket-das <saiketsd23@gmail.com>
2022-04-17 12:23:27 +0530

    room update and delete route added

--- controllers/roomControllers.js
+++ controllers/roomControllers.js
@@ -35,4 +35,98 @@ const newRoom = async (req, res) => {
   }
 };
 
-export { allRooms, newRoom };
+// Get room details  => /api/rooms/:id
+
+const getSingleRoom = async (req, res) => {
+  try {
+    const room = await Room.findById(req.query.id);
+
+    if (!room) {
+      return res.status(404).json({
+        success: false,
+        error: 'Room not found',
+      });
+    }
+
+    res.status(201).json({
+      success: true,
+      room,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+// Update room   => /api/rooms/:id
+
+const updateRoom = async (req, res) => {
+  try {
+    let room = await Room.findById(req.query.id);
+
+    if (!room) {
+      return res.status(404).json({
+        success: false,
+        error: 'Room not found',
+      });
+    }
+
+
+    room = await Room.findByIdAndUpdate(req.query.id, req.body, {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
+    })
+
+    res.status(201).json({
+      success: true,
+      room,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+
+
+
+
+// Delete room   => /api/rooms/:id
+
+const deleteRoom = async (req, res) => {
+  try {
+    const room = await Room.findById(req.query.id);
+
+    if (!room) {
+      return res.status(404).json({
+        success: false,
+        error: 'Room not found',
+      });
+    }
+
+
+    await room.remove();
+
+    res.status(201).json({
+      success: true,
+      message: 'Room deleted successfully',
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+
+
+
+
+
+export { allRooms, newRoom, getSingleRoom , updateRoom, deleteRoom };
